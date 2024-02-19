de74c460871f138e581cb23e4514577570deaaf4
Abdullah Al Mamun <abdullah914115@gmail.com>
2024-02-19 10:29:27 +0600

    add scripts to open phpstrom on current working directory

---
+++ misc/phpstrom.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+project_path="$PWD"
+nohup phpstorm "$project_path" >/dev/null 2>&1 &
+
+# Guide to setup
+# save this file on root directory ~
+# chmod +x phpstrom.sh
+# jump your project directory and exec this file with : ~/phpstrom.sh
+
+
+## with the help of this file you will able to open phpstrom as like code .
